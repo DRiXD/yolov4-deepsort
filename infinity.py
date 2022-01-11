d6651c94d1eb9579a0659b253e1fe93a85b46753
denisRei <39619011+DRiXD@users.noreply.github.com>
2022-01-11 20:16:51 +0100

    changed command to source conda-env

--- infinity.py
+++ infinity.py
@@ -23,7 +23,7 @@ while True:
             output_filename = f"processed_{now}"
             file_to_process= f"{DATA_DIR}/{file}"
 
-            os.system(f"python ./yolov4-deepsort/object_tracker.py --video {file_to_process} --output ./yolov4-deepsort/outputs/{output_filename}.avi --model yolov4 --dont_show --info")
+            os.system(f"conda activate yolov4-gpu && python ./yolov4-deepsort/object_tracker.py --video {file_to_process} --output ./yolov4-deepsort/outputs/{output_filename}.avi --model yolov4 --dont_show --info")
 
         print("All new files are processed now.")
        
