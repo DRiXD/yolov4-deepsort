aefb38707d4429cf14c08b110ad0ec34c57149d9
denisRei <39619011+DRiXD@users.noreply.github.com>
2022-01-12 15:25:13 +0100

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}')
 . $CONDA_PATH/etc/profile.d/conda.sh
 
 conda activate yolov4-gpu
- 
-python ./yolov4-deepsort/infinity.py
+
+cd yolov4-deepsort/
+
+python infinity.py
